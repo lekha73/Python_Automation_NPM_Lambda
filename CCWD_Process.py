0ecabdd19706f2dc37f445e3b35815f2fcd8df0a
lekha73 <45451197+lekha73@users.noreply.github.com>
2021-07-07 12:35:36 +0530

    Update CCWD_Process.py

--- CCWD_Process.py
+++ CCWD_Process.py
@@ -7,6 +7,12 @@ import time
 import sys
 
 boto3.setup_default_session(profile_name='RulesEngineProd')
+sfn = boto3.client('stepfunctions')
+dynamodb = boto3.client('dynamodb')
+s3 = boto3.resource('s3')
+lamb = boto3.client('lambda')
+s3_client = boto3.client('s3')
+
 #prerequisite getting today's & yesterday's date to update the sheet
 today = date.today()
 yesterday = today - timedelta(days = 1)
@@ -19,16 +25,16 @@ processed_date = processed_date.strftime("%Y/%m/%d")
 processed_date = processed_date.replace("/", "")
 
 #reading file add engine detail to read xlsb binary file (not required for xlsx file)
-filename = 'Allowable Cost Center Create -06252021'
+filename = 'Filename'
 filename_binary = filename + '.xlsb'
 file = pd.read_excel(filename_binary, engine='pyxlsb')
 print(file)
 #removes empty rows under column name Cost Center
-file.dropna(subset = ["Cost Center"], inplace=True)
+file.dropna(subset = ["Center"], inplace=True)
 #axis 1 denotes column. it removes all the empty columns
 file.dropna(how='all', axis=1, inplace=True)
 #sometimes we receive sub org with decimal .0, below line is to remove decimal points
-file['Sup Org'] = file['Sup Org'].apply(int)
+file['Org'] = file['Org'].apply(int)
 file.drop(file.columns[file.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 
 #modifying excel sheet - Action items
@@ -41,8 +47,8 @@ file.loc[file.Action == "delimit", 'Effective Start Date'] = yesterday
 file.loc[file.Action == "delimit", 'Effective End Date'] = yesterday
 file.loc[file.Action == "Add", 'Effective Start Date'] = today
 file.loc[file.Action == "Add", 'Effective End Date'] = "12/31/9999"
-file['Cost Center'] = file['Cost Center'].astype(str).replace('\.0', '', regex=True)
-file['Cost Center'] = file['Cost Center'].apply(lambda x: '{0:0>10}'.format(x))
+file['Center'] = file['Center'].astype(str).replace('\.0', '', regex=True)
+file['Center'] = file['Center'].apply(lambda x: '{0:0>10}'.format(x))
 print(file)
 #print(file.info())
 
@@ -51,8 +57,6 @@ updated_file = filename+"-"+processed_date+'.xlsx'
 file.to_excel(updated_file, index=False)
 
 #boto3 sfn steps to check last executed status and date
-sfn = boto3.client('stepfunctions')
-
 def stepfn_details(stepfunction_name):
     print(stepfunction_name)
     response = sfn.list_executions(
@@ -66,28 +70,29 @@ def stepfn_details(stepfunction_name):
     if exec_status == "FAILED":
         sys.exit()
 
-stepfn_details('arn:aws:states:us-east-1:136703301440:stateMachine:stepfn_name')
+stepfn_details('arn:aws:states:us-east-1:AccID:stateMachine:fnname')
+
+
+db_details = dynamodb.describe_table(TableName='T.name')
+print("table item count " +str(db_details['Table']['ItemCount']))
+
 #upload modified xlsx sheet t s3
-s3 = boto3.resource('s3')
-s3_response = s3.meta.client.upload_file(updated_file, 's3-bucket', 'sub-folder/'+updated_file)
-print(s3_response)
+s3.meta.client.upload_file(updated_file, 's3bucketname', 'path/'+updated_file)
 print("Uploaded to s3! Added sleep time to proceed with lambda process")
 time.sleep(20)
-lamb = boto3.client('lambda')    
-env_var = "subfolder/"+updated_file
+    
+env_var = "path/"+updated_file
 try:
     lamb_response = lamb.update_function_configuration(
-        FunctionName='lambda_name',
+        FunctionName='Lambdaname',
         Environment={
             'Variables': {
                 'tempInputPath': env_var,
-                'chunk_size': '1000',
-                'env': 'Prod',
-                'localenv': 'false',            
+                'chunk_size': '1000'             
             }
         }
     )
-    print(lamb_response)
+    print("updated lambda environment variables")
 except Exception as e:
     print(e)
 try:
@@ -98,7 +103,7 @@ except Exception as e:
 print("Started lambda!wait for few mins to complete")
 def invoke_lambda():
     time.sleep(360)
-    status = stepfn_details('arn:aws:states:us-east-1:136703301440:stateMachine:stfn')
+    status = stepfn_details('arn:aws:states:us-east-1:AccID:stateMachine:fnname')
     if status == "SUCCEEDED":
         try:
             print("step function succeeded")
@@ -117,24 +122,24 @@ def invoke_lambda():
 invoke_lambda()
 print("Downloading S3 files")
 time.sleep(20)
-s3_client = boto3.client('s3')
+
 def download_S3file(path, local_filename):
     s3 = boto3.resource('s3')
     s3File = s3_client.list_objects_v2(
-        Bucket='s3-bucket',
+        Bucket='s3bucket',
         Prefix=path
     )   
     S3Filename = s3File['Contents'][-1]['Key']
     print (s3File['Contents'][-1]['LastModified'])
     print(S3Filename)
-    s3.meta.client.download_file('s3-bucket', S3Filename, local_filename)
+    s3.meta.client.download_file('s3bucket', S3Filename, local_filename)
     return S3Filename
-download_S3file('subfolder/', 'audit.txt')
+download_S3file('S3path/', 'audit.txt')
 print("downloaded audit file")
-S3Filename=download_S3file('subfolder/', 'CostCenter.xml')
+S3Filename=download_S3file('S3path/', 'CostCenter.xml')
 print("downloaded final xml file")  
 def delete_S3File(S3Filename):
-    response = s3_client.delete_object(Bucket='s3-bucket',Key=S3Filename)
+    response = s3_client.delete_object(Bucket='S3bucket',Key=S3Filename)
     print("S3 File deleted")
 print("deleting xml file "+S3Filename)
 delete_S3File(S3Filename)
